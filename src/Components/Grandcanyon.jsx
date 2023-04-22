a4a73219151e8b6330b78631cf9ca9a4627f6274
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-04-22 01:21:16 -0300

    Iniciando criação da função de textos dos componentes

---
+++ src/Components/Grandcanyon.jsx
@@ -0,0 +1,11 @@
+import styles from "../Components/css/Grandcanyon.module.css"
+
+function Grandcanyon (){
+    return(
+        <>
+        <section className={styles.body}>
+        
+        </section>
+        </>
+    )
+}
