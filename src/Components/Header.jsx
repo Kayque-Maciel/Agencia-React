4a44a6da50cb833389ac6f4ae6f1eb9ab66362a4
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-04-21 23:36:21 -0300

    Header finalizado

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,30 @@
+import styles from "../Components/Header.module.css"
 import logo from "../Components/Images/viagens.jpg"
+import lupa from "../Components/Images/lupa.png"
+import mala from "../Components/Images/mala.jpg"
 
 function Header(){
     return(
         <>
         <header>
-            <img src={logo} alt="logo"  />
+            <img src={logo} alt="logo" className={styles.logo}/>
+            <nav>
+                <ul className={styles.navlist}>
+                    <li><a href="/about">Home</a></li>
+                    <li><a href="/contact">Grand Canyon</a></li>
+                    <li><a href="/press">Escócia</a></li>
+                    <li><a href="/press">Muralhas da China</a></li>
+                    <li><a href="/press">Arábia</a></li>
+                </ul> 
+            </nav>
+
+            <input type="text" id={styles.search}/>
+            <img src={lupa} alt="lupa" className={styles.lupa}/>
         </header>
+
+        <section>
+            <img src={mala} alt="mala" className={styles.mala}/>
+        </section>
         </>
     );
 }
