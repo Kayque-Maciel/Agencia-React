00809ee026986ab54374d473e32c84e1a9f55957
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-04-21 22:03:31 -0300

    Iniciando header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import logo from "../Components/Images/viagens.jpg"
+
+function Header(){
+    return(
+        <>
+        <header>
+            <img src={logo} alt="logo"  />
+        </header>
+        </>
+    );
+}
+
+export default Header;
