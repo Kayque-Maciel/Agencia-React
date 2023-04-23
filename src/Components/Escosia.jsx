661b1fa8e61d7f40ed1c359f9aff847a1cdc1d8e
Kayque Maciel <94768852+Kayque-Maciel@users.noreply.github.com>
2023-04-23 00:23:11 -0300

    Finalizando a página Escócia

--- src/Components/Escosia.jsx
+++ src/Components/Escosia.jsx
@@ -7,8 +7,8 @@ function Suecia (){
         <>
         <section className={styles.body}>
             <TextosComponentes 
-            lugar="Grand Canyon"
-            texto="Turismo na Escócia, localizado ao lado da"
+            lugar="Escócia"
+            texto="Turismo na Escócia, localizado ao lado da Inglaterra, fica a Escócia, um dos países pertecentes ao Reino Unido. Que traz consigo uma grande carga histórica, lindas paisagens naturais e contruções antigas, como seus castelos medievais."
             logo_texto={logo}
             />
         </section>
